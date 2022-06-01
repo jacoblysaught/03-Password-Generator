70e2926e14200a44be778fd833c41fff69e86a9b
Jacob Lysaught <jacob.lysaught@gmail.com>
2022-06-01 12:14:13 -0600

    created function to generate random character

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,26 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// returns random characters including alphanumeric and special characters 
+function generatePassword() {
+  return String.fromCharCode(Math.floor(Math.random() * 77) + 34);
+}
+
 // Write password to the #password input
-function writePassword() {
+function writePassword(num) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+
+  for (var i = 0; i < num; i++) {
+    password += generatePassword();
+  }
+  return password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
